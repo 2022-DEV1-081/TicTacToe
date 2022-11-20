4d8f856b383165bd3a473a26fe88638a2a82b3cb
2022-DEV1-081 <2022dev1081@gmail.com>
2022-11-21 01:14:16 +0530

    refactor: Extract service initialization to separate method in test class

--- src/test/java/com/game/tictactoe/service/GameServiceTests.java
+++ src/test/java/com/game/tictactoe/service/GameServiceTests.java
@@ -2,6 +2,7 @@ package com.game.tictactoe.service;
 
 import com.game.tictactoe.domain.Player;
 import com.game.tictactoe.exception.InvalidTurnException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -11,18 +12,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(MockitoJUnitRunner.class)
 public class GameServiceTests {
 
+    private GameService gameService;
+
+    @Before
+    public void setUp() {
+        gameService = new GameService();
+    }
+
     @Test
     public void playerXShouldAlwaysGoFirst() {
-
-        GameService gameService = new GameService();
 
         assertThat(gameService.playGame(Player.X)).isEqualTo("Player X moved first");
     }
 
     @Test(expected = InvalidTurnException.class)
     public void shouldThrowInvalidTurnExceptionIfOMovesFirst() {
-
-        GameService gameService = new GameService();
 
         gameService.playGame(Player.O);
     }
