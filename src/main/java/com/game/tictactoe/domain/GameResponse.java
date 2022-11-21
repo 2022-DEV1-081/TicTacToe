145b3b45fdb47c60d09847e311e890444eb67d09
2022-DEV1-081 <2022dev1081@gmail.com>
2022-11-21 10:57:25 +0530

    feat: Declare winner if any one of three rows is occupied by same player

--- src/main/java/com/game/tictactoe/domain/GameResponse.java
+++ src/main/java/com/game/tictactoe/domain/GameResponse.java
@@ -2,14 +2,24 @@ package com.game.tictactoe.domain;
 
 public class GameResponse {
 
+    private String result;
     private String status;
     private Player nextPlayer;
     private Player currentPlayer;
+
+    public GameResponse(String result, String status) {
+        this.result = result;
+        this.status = status;
+    }
 
     public GameResponse(String status, Player nextPlayer, Player currentPlayer) {
         this.status = status;
         this.nextPlayer = nextPlayer;
         this.currentPlayer = currentPlayer;
+    }
+
+    public String getResult() {
+        return result;
     }
 
     public String getStatus() {
