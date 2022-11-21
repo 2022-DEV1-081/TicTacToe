ed1963c5e7d4b72403eb4d5d22f52871206c63af
2022-DEV1-081 <2022dev1081@gmail.com>
2022-11-21 14:25:41 +0530

    refactor: Exclude null fields from game response object

--- src/main/java/com/game/tictactoe/domain/GameResponse.java
+++ src/main/java/com/game/tictactoe/domain/GameResponse.java
@@ -1,8 +1,10 @@
 package com.game.tictactoe.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GameResponse {
 
     private String result;
